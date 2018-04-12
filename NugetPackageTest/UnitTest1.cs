7e84a23d50ccab1ff29cc64ad5267fd1cd7f84dc
Sameera <www.sameera@gmail.com>
2018-04-12 22:33:53 +0530

    Resolved an issue with Nuget Package

---
+++ NugetPackageTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using SuiteTalk;
+
+namespace NugetPackageTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+   
+        }
+    }
+}
