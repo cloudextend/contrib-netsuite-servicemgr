6541990415fd305b650bae8b2a1a20babd652125
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-10-31 15:06:30 +0530

    add bootstrap and style the app a little

--- src/app/login-types/login-types.component.ts
+++ src/app/login-types/login-types.component.ts
@@ -15,19 +15,53 @@ export class LoginTypesComponent implements OnInit {
     showTBA: boolean;
     showSSO: boolean;
 
+    accounts: object[];
+    selectedAccount: object;
+
     constructor(
         private route: ActivatedRoute,
         private http: Http
     ) {
+        const loginMethods = {
+            'celigo-basic': 'basic',
+            'celigo-tba': 'tba',
+            'celigo-sso': 'sso',
+        };
+        this.selectedAccount = {
+            accountName: 'choose an account',
+            basic: false,
+            tba: false,
+            sso: false
+        };
+
         this.userEmail = this.route.snapshot.queryParams.email;
         this.http.get('https://00a817a2.ap.ngrok.io/api/netsuite/2.0/auth/type?' +
             `email=${this.route.snapshot.queryParams.email}`
         )
         .map(resp => resp.json())
-        .subscribe(({ basic, tba, sso}) => {
-            this.showBasic = basic;
-            this.showTBA = tba;
-            this.showSSO = sso;
+        .subscribe((response) => {
+            const { accounts } = response;
+
+            this.accounts = accounts.map(({account, methods}) => {
+                const loginMap = methods.reduce((map, {method, enabled }) => {
+                    map[loginMethods[method]] = enabled;
+
+                    return map;
+                }, {});
+
+                return {
+                    accountName: account.name,
+                    ...loginMap
+                };
+            });
+
+            this.selectedAccount = this.accounts[0];
+
+            console.log(this.accounts);
+
+            // this.showBasic = basic;
+            // this.showTBA = tba;
+            // this.showSSO = sso;
         });
     }
 
