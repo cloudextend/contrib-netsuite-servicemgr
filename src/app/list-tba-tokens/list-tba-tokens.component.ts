27b96bd6ad325148c545777f839da09aaafc72de
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-11-08 18:34:46 +0530

    fix redirect after successful login for list-tba-tokens-view

--- src/app/list-tba-tokens/list-tba-tokens.component.ts
+++ src/app/list-tba-tokens/list-tba-tokens.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { AuthUserPreferencesService, LoginStates } from 'lib-client-auth-netsuite';
+
+import { StorageService } from '../storage.service';
+import { environment } from '../../environments/environment';
+
 @Component({
     selector: 'app-list-tba-tokens',
     templateUrl: './list-tba-tokens.component.html',
@@ -8,16 +13,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListTbaTokensComponent implements OnInit {
     pin: string;
+    userEmail: string;
 
     constructor(
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private storage: StorageService,
+        private userPreferenceService: AuthUserPreferencesService,
     ) { }
 
     ngOnInit() {
         this.pin = this.route.snapshot.queryParams.pin;
+        this.userEmail = this.userPreferenceService.getDefaultEmail();
     }
 
-    onLoginStateChange(event) {
-        console.log(event);
+    private redirectToCEXLApp() {
+        window.location.href = environment.urls.cexlApp;
+    }
+
+    onLoginStateChange({state, data}) {
+        console.log({state, data});
+        data.credentialType = 'celigo-tba';
+
+        data.user = {...data.user, email: this.userEmail};
+
+        console.log('onLoginStateChange, event:', {state, data});
+
+        if (state === LoginStates.Success) {
+            this.storage.set('celigo_cexl_session_data', data);
+            this.redirectToCEXLApp();
+        }
     }
 }
