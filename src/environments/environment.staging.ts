0e45bf653d94997bbb51ca4eb79684194d64637d
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-12-04 15:16:41 +0530

    setup env variables for staging and prod deployments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,14 +1,18 @@
 export const environment = {
     production: true,
     urls: {
-        authAPI: {
+        backend: {
             base: 'https://staging.smartclient.io',
 
             basicAuth: '/api/netsuite/2.0/auth',
             tbaAuth: '/api/netsuite/2.0/auth',
 
             loginMethods: '/api/netsuite/2.0/auth/type',
-            initiateSSO: '/api/netsuite/2.0/auth/initiate-sso'
+            initiateSSO: '/api/netsuite/2.0/auth/initiate-sso',
+
+            leadCreation: '/integration/salesforce',
+            trialActivation: '/license/activateTrial',
+            licenseCheck: '/license/check',
         },
         cexlApp: 'https://staging.smartclient.io/'
     }
