2609328d7a27ba19f70d40d3f8d6f643cf1bca8a
Sameera <www.sameera@gmail.com>
2019-09-18 19:31:30 +0530

    Nuget Package for Three-step Auth

---
+++ Celigo.ServiceManager.NetSuite.TSA/src/ServiceCollection.extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celigo.ServiceManager.NetSuite.TSA
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddThreeStepAuthorizationSupport(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddOptions()
+                .Configure<TokenServiceOptions>(configuration.GetSection("Celigo:NetSuite:TBA"))
+                .AddSingleton<TokenService, DefaultTokenService>();
+        }
+    }
+}
