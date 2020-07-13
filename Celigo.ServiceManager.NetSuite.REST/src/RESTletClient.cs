00ac57079bc4a9839ca64299f7d2e4cf95cb77aa
Sameera Perera <www.sameera@gmail.com>
2020-07-13 22:21:22 +0530

    Remove conflicting constructor

--- Celigo.ServiceManager.NetSuite.REST/src/RESTletClient.cs
+++ Celigo.ServiceManager.NetSuite.REST/src/RESTletClient.cs
@@ -36,17 +36,6 @@ namespace Celigo.ServiceManager.NetSuite.REST
         private const string _restletRelativePath = ".restlets.api.netsuite.com/app/site/hosting/restlet.nl";
         private const string _scriptParamName = "script";
         private const string _deployParamName = "deploy";
-        
-        public RestletClient(HttpClient httpClient, IOptions<RestClientOptions> options, RestletConfig restlet) 
-            : base(httpClient, options)
-        {
-            _ = restlet.Deploy ??
-                throw new ArgumentNullException($"{nameof(RestletConfig)}.{nameof(RestletConfig.Deploy)}");
-            _ = restlet.Script ??
-                throw new ArgumentNullException($"{nameof(RestletConfig)}.{nameof(RestletConfig.Script)}");
-            
-            _restlet = restlet;
-        }
 
         public RestletClient(HttpClient httpClient, IOptions<RestClientOptions> options, IOptions<RestletConfig> restlet)
             : base(httpClient, options) 
