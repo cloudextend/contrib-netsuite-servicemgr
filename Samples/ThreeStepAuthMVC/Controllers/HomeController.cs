46d03416cb982b441dedfcb948e59bafbb8a916a
srikanthrokkam <Srikanththemonarch@gmail.com>
2022-05-31 12:31:45 +0530

    Addressed review comment

--- Samples/ThreeStepAuthMVC/Controllers/HomeController.cs
+++ Samples/ThreeStepAuthMVC/Controllers/HomeController.cs
@@ -29,9 +29,11 @@ public class HomeController : Controller
         return View("Authorize");
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, 
+        NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? 
+            HttpContext.TraceIdentifier });
     }
 }
--- Samples/ThreeStepAuthMVC/Controllers/TsaLoginController.cs
+++ Samples/ThreeStepAuthMVC/Controllers/TsaLoginController.cs
@@ -10,7 +10,9 @@ namespace ThreeStepAuthMVC.Controllers
         private readonly IRequestTokenService _requestTokenService;
         private readonly IRevokeTokenService _revokeTokenService;
 
-        public TsaLoginController(IAccessTokenService accessTokenService, IRequestTokenService requestTokenService, IRevokeTokenService revokeTokenService)
+        public TsaLoginController(IAccessTokenService accessTokenService,
+            IRequestTokenService requestTokenService,
+            IRevokeTokenService revokeTokenService)
         {
             _accessTokenService = accessTokenService;
             _requestTokenService = requestTokenService;
@@ -18,24 +20,35 @@ namespace ThreeStepAuthMVC.Controllers
         }
 
         [HttpGet("request-token")]
-        public async Task<ActionResult<RequestTokenResponse>> RequestToken(string account)
+        public async Task<ActionResult<RequestTokenResponse>> 
+            RequestToken(string account)
         {
             var response = await _requestTokenService.GetRequestToken(account);
-            return (response.Error == null) ? Ok(response) : StatusCode((int)response.Error.StatusCode, response);
+            return (response.Error == null) ? Ok(response) : 
+                StatusCode((int)response.Error.StatusCode, response);
         }
 
         [HttpGet("authorized-token")]
-        public async Task<ActionResult<AccessTokenResponse>> AuthorizeToken(string account, string requestToken, string tokenSecret, string verifier)
+        public async Task<ActionResult<AccessTokenResponse>> AuthorizeToken(
+            string account, string requestToken, string tokenSecret, 
+            string verifier)
         {
-            var response = await _accessTokenService.GetAccessToken(account, requestToken, tokenSecret, verifier);
-            return (response.Error == null) ? Ok(response) : StatusCode((int)response.Error.StatusCode, response);
+            var response = await _accessTokenService.GetAccessToken(
+                account, requestToken, 
+                tokenSecret, verifier);
+            
+            return (response.Error == null) ? Ok(response) : 
+                StatusCode((int)response.Error.StatusCode, response);
         }
 
         [HttpGet("revoke-token")]
-        public async Task<ActionResult> RevokeToken(string account, string token, string tokenSecret)
+        public async Task<ActionResult> RevokeToken(string account, 
+            string token, string tokenSecret)
         {
-            var response = await _revokeTokenService.RevokeToken(account, token, tokenSecret);
-            return response.Error == null ? Ok(response) : StatusCode((int)response.Error.StatusCode, response);
+            var response = await _revokeTokenService.RevokeToken(account,
+                token, tokenSecret);
+            return response.Error == null ? Ok(response) : 
+                StatusCode((int)response.Error.StatusCode, response);
         }
     }
 }
