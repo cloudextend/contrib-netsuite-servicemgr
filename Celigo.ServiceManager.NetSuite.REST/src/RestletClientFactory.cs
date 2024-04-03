566e13f80ee8d7b8005f707b6c260b96090db50e
Srikanth <155434579+srikanth-cloudextend@users.noreply.github.com>
2024-04-03 13:17:44 +0530

    DMP-4197 Added support for HTTP timeout with custom value while using RestletClient
    (cherry picked from commit 0b9d9e42fd371c0379759f87c142fa81acdef98b)

--- Celigo.ServiceManager.NetSuite.REST/src/RestletClientFactory.cs
+++ Celigo.ServiceManager.NetSuite.REST/src/RestletClientFactory.cs
@@ -38,6 +38,12 @@ namespace Celigo.ServiceManager.NetSuite.REST
             if (_registry.TryGetValue(restletName, out var restlet))
             {
                 var httpClient = _clientFactory.CreateClient(restletName);
+                httpClient.Timeout =
+                    string.IsNullOrWhiteSpace(restlet.HttpTimeOutInMinutes)
+                        ? TimeSpan.FromMinutes(
+                            Convert.ToInt32(restlet.HttpTimeOutInMinutes))
+                        : TimeSpan.FromMinutes(2);
+
                 return new RestletClient(httpClient, _restClientOptions, Options.Create(restlet));
             }
             else
